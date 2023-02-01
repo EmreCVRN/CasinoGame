69b638cc04ee283a1d837306a2358f241a8e143a
EmreCVRN <98412302+EmreCVRN@users.noreply.github.com>
2023-02-02 00:22:45 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace Project_76742
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.lbl_slotmachine = new System.Windows.Forms.Label();
+            this.lbl_goodtoseeyou = new System.Windows.Forms.Label();
+            this.lbl_balance = new System.Windows.Forms.Label();
+            this.lbl_wins = new System.Windows.Forms.Label();
+            this.lbl_loss = new System.Windows.Forms.Label();
+            this.lbl_spinPrice = new System.Windows.Forms.Label();
+            this.btn_play = new System.Windows.Forms.Button();
+            this.btn_newgame = new System.Windows.Forms.Button();
+            this.textBox_spinPrice = new System.Windows.Forms.TextBox();
+            this.textBox_balance = new System.Windows.Forms.TextBox();
+            this.textBox_add = new System.Windows.Forms.TextBox();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btn_info = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Project_76742.Properties.Resources.imgbin_casino_gambling_slot_machine_icon_diamond_chips_1siX49jPrGEKifb1kWNyzSchP1;
+            this.pictureBox1.Location = new System.Drawing.Point(23, 140);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Project_76742.Properties.Resources.imgbin_casino_gambling_slot_machine_icon_diamond_chips_1siX49jPrGEKifb1kWNyzSchP1;
+            this.pictureBox2.Location = new System.Drawing.Point(132, 140);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Project_76742.Properties.Resources.imgbin_casino_gambling_slot_machine_icon_diamond_chips_1siX49jPrGEKifb1kWNyzSchP1;
+            this.pictureBox3.Location = new System.Drawing.Point(253, 140);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Project_76742.Properties.Resources.imgbin_casino_gambling_slot_machine_icon_diamond_chips_1siX49jPrGEKifb1kWNyzSchP1;
+            this.pictureBox4.Location = new System.Drawing.Point(373, 140);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // lbl_slotmachine
+            // 
+            this.lbl_slotmachine.AutoSize = true;
+            this.lbl_slotmachine.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_slotmachine.Font = new System.Drawing.Font("Comic Sans MS", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_slotmachine.ForeColor = System.Drawing.Color.Lime;
+            this.lbl_slotmachine.Location = new System.Drawing.Point(23, 25);
+            this.lbl_slotmachine.Name = "lbl_slotmachine";
+            this.lbl_slotmachine.Size = new System.Drawing.Size(158, 32);
+            this.lbl_slotmachine.TabIndex = 4;
+            this.lbl_slotmachine.Text = "Slot Machine\r\n";
+            // 
+            // lbl_goodtoseeyou
+            // 
+            this.lbl_goodtoseeyou.AutoSize = true;
+            this.lbl_goodtoseeyou.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_goodtoseeyou.Font = new System.Drawing.Font("Comic Sans MS", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_goodtoseeyou.ForeColor = System.Drawing.Color.Red;
+            this.lbl_goodtoseeyou.Location = new System.Drawing.Point(277, 25);
+            this.lbl_goodtoseeyou.Name = "lbl_goodtoseeyou";
+            this.lbl_goodtoseeyou.Size = new System.Drawing.Size(271, 38);
+            this.lbl_goodtoseeyou.TabIndex = 5;
+            this.lbl_goodtoseeyou.Text = " :) Good To See you \n";
+            this.lbl_goodtoseeyou.UseCompatibleTextRendering = true;
+            // 
+            // lbl_balance
+            // 
+            this.lbl_balance.AutoSize = true;
+            this.lbl_balance.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_balance.Font = new System.Drawing.Font("Comic Sans MS", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_balance.ForeColor = System.Drawing.Color.Black;
+            this.lbl_balance.Location = new System.Drawing.Point(12, 89);
+            this.lbl_balance.Name = "lbl_balance";
+            this.lbl_balance.Size = new System.Drawing.Size(144, 32);
+            this.lbl_balance.TabIndex = 6;
+            this.lbl_balance.Text = "$: Balance ";
+            this.lbl_balance.Click += new System.EventHandler(this.lbl_balance_Click);
+            // 
+            // lbl_wins
+            // 
+            this.lbl_wins.AutoSize = true;
+            this.lbl_wins.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_wins.Font = new System.Drawing.Font("Comic Sans MS", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_wins.ForeColor = System.Drawing.Color.GhostWhite;
+            this.lbl_wins.Location = new System.Drawing.Point(44, 286);
+            this.lbl_wins.Name = "lbl_wins";
+            this.lbl_wins.Size = new System.Drawing.Size(101, 32);
+            this.lbl_wins.TabIndex = 7;
+            this.lbl_wins.Text = "0: Wins";
+            // 
+            // lbl_loss
+            // 
+            this.lbl_loss.AutoSize = true;
+            this.lbl_loss.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_loss.Font = new System.Drawing.Font("Comic Sans MS", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_loss.ForeColor = System.Drawing.Color.Snow;
+            this.lbl_loss.Location = new System.Drawing.Point(44, 318);
+            this.lbl_loss.Name = "lbl_loss";
+            this.lbl_loss.Size = new System.Drawing.Size(95, 32);
+            this.lbl_loss.TabIndex = 8;
+            this.lbl_loss.Text = "0: Loss";
+            // 
+            // lbl_spinPrice
+            // 
+            this.lbl_spinPrice.AutoSize = true;
+            this.lbl_spinPrice.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_spinPrice.Font = new System.Drawing.Font("Comic Sans MS", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_spinPrice.ForeColor = System.Drawing.Color.LightGreen;
+            this.lbl_spinPrice.Location = new System.Drawing.Point(281, 265);
+            this.lbl_spinPrice.Name = "lbl_spinPrice";
+            this.lbl_spinPrice.Size = new System.Drawing.Size(124, 32);
+            this.lbl_spinPrice.TabIndex = 9;
+            this.lbl_spinPrice.Text = "Spin Price";
+            // 
+            // btn_play
+            // 
+            this.btn_play.BackColor = System.Drawing.Color.Pink;
+            this.btn_play.Font = new System.Drawing.Font("Comic Sans MS", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_play.Location = new System.Drawing.Point(87, 360);
+            this.btn_play.Name = "btn_play";
+            this.btn_play.Size = new System.Drawing.Size(141, 73);
+            this.btn_play.TabIndex = 10;
+            this.btn_play.Text = "Play";
+            this.btn_play.UseVisualStyleBackColor = false;
+            this.btn_play.Click += new System.EventHandler(this.btn_play_Click);
+            // 
+            // btn_newgame
+            // 
+            this.btn_newgame.BackColor = System.Drawing.Color.Pink;
+            this.btn_newgame.Font = new System.Drawing.Font("Comic Sans MS", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_newgame.Location = new System.Drawing.Point(258, 360);
+            this.btn_newgame.Name = "btn_newgame";
+            this.btn_newgame.Size = new System.Drawing.Size(141, 73);
+            this.btn_newgame.TabIndex = 11;
+            this.btn_newgame.Text = "New Game";
+            this.btn_newgame.UseVisualStyleBackColor = false;
+            this.btn_newgame.Click += new System.EventHandler(this.btn_newgame_Click);
+            // 
+            // textBox_spinPrice
+            // 
+            this.textBox_spinPrice.Location = new System.Drawing.Point(281, 320);
+            this.textBox_spinPrice.Name = "textBox_spinPrice";
+            this.textBox_spinPrice.Size = new System.Drawing.Size(117, 22);
+            this.textBox_spinPrice.TabIndex = 12;
+            // 
+            // textBox_balance
+            // 
+            this.textBox_balance.BackColor = System.Drawing.Color.Black;
+            this.textBox_balance.Font = new System.Drawing.Font("Comic Sans MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_balance.ForeColor = System.Drawing.Color.Red;
+            this.textBox_balance.Location = new System.Drawing.Point(162, 86);
+            this.textBox_balance.Name = "textBox_balance";
+            this.textBox_balance.Size = new System.Drawing.Size(66, 35);
+            this.textBox_balance.TabIndex = 13;
+            this.textBox_balance.Text = "100";
+            this.textBox_balance.TextChanged += new System.EventHandler(this.textBox_balance_TextChanged);
+            // 
+            // textBox_add
+            // 
+            this.textBox_add.Location = new System.Drawing.Point(288, 441);
+            this.textBox_add.Name = "textBox_add";
+            this.textBox_add.Size = new System.Drawing.Size(111, 22);
+            this.textBox_add.TabIndex = 14;
+            this.textBox_add.TextChanged += new System.EventHandler(this.textBox_add_TextChanged);
+            // 
+            // btn_add
+            // 
+            this.btn_add.BackColor = System.Drawing.Color.SpringGreen;
+            this.btn_add.Font = new System.Drawing.Font("Comic Sans MS", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_add.Location = new System.Drawing.Point(412, 427);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(136, 36);
+            this.btn_add.TabIndex = 15;
+            this.btn_add.Text = "Add Balance";
+            this.btn_add.UseVisualStyleBackColor = false;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btn_info
+            // 
+            this.btn_info.Font = new System.Drawing.Font("Comic Sans MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_info.ForeColor = System.Drawing.Color.LawnGreen;
+            this.btn_info.Location = new System.Drawing.Point(172, 226);
+            this.btn_info.Name = "btn_info";
+            this.btn_info.Size = new System.Drawing.Size(103, 44);
+            this.btn_info.TabIndex = 16;
+            this.btn_info.Text = "INFO";
+            this.btn_info.UseVisualStyleBackColor = true;
+            this.btn_info.Click += new System.EventHandler(this.btn_info_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Project_76742.Properties.Resources.casino;
+            this.ClientSize = new System.Drawing.Size(560, 475);
+            this.Controls.Add(this.btn_info);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.textBox_add);
+            this.Controls.Add(this.textBox_balance);
+            this.Controls.Add(this.textBox_spinPrice);
+            this.Controls.Add(this.btn_newgame);
+            this.Controls.Add(this.btn_play);
+            this.Controls.Add(this.lbl_spinPrice);
+            this.Controls.Add(this.lbl_loss);
+            this.Controls.Add(this.lbl_wins);
+            this.Controls.Add(this.lbl_balance);
+            this.Controls.Add(this.lbl_goodtoseeyou);
+            this.Controls.Add(this.lbl_slotmachine);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(3)))));
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label lbl_slotmachine;
+        private System.Windows.Forms.Label lbl_goodtoseeyou;
+        private System.Windows.Forms.Label lbl_balance;
+        private System.Windows.Forms.Label lbl_wins;
+        private System.Windows.Forms.Label lbl_loss;
+        private System.Windows.Forms.Label lbl_spinPrice;
+        private System.Windows.Forms.Button btn_play;
+        private System.Windows.Forms.Button btn_newgame;
+        private System.Windows.Forms.TextBox textBox_spinPrice;
+        private System.Windows.Forms.TextBox textBox_balance;
+        private System.Windows.Forms.TextBox textBox_add;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btn_info;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_76742
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rnd = new Random();
+        int a, b, c, d,s, move, wins, loss, balance; // adding variables which ones we will need it 
+
+        private void lbl_balance_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_newgame_Click(object sender, EventArgs e)
+        {
+            textBox_spinPrice.Text = "Slot Machine";
+            textBox_spinPrice.Enabled = true;
+            textBox_spinPrice.Text = "";
+            textBox_spinPrice.BackColor = Color.White;
+            textBox_spinPrice.Focus();
+            btn_play.Enabled = true;
+            btn_play.Text = "Play";
+            lbl_balance.Text = "Balance :$";
+        }
+
+        private void btn_info_Click(object sender, EventArgs e)
+        {
+            if (btn_info.Text == "") ;
+
+            MessageBox.Show("You will have $100 start money " +
+                "\n If you want to win the game you have to get at least 3 pictures same otherwise you will lose " +
+                "\n When you have 3 pictures same you will earn double " +
+                "\n When you have 4 pictures same you will earn 4 times from your spin money " +
+                "\n If you lose the game you will lose amonut of your spin money" +
+                "\n Good Luck Have Fun !!!"); 
+        }
+
+        private void textBox_balance_TextChanged(object sender, EventArgs e)
+        {
+            4
+        }
+
+        // we need to create game variable...
+
+        void Game_Result()
+        {
+           
+            balance = Convert.ToInt32(textBox_balance.Text);
+
+            if (a == b && b == c && c == d) // first win condition to get 3 pictures same
+               
+            {
+                wins++;
+                lbl_wins.Text = "Wins : " + wins; // if users win the game wins label keep the score
+                balance += Convert.ToInt32(textBox_spinPrice.Text) * 2; // if users win the game get double price from spin price
+                textBox_balance.Text = balance.ToString(); // it will change the balance
+                btn_play.Text = "Continue Playing :";
+                lbl_slotmachine.Text = "You Won ";
+
+            }
+            else if ((a == b && b ==c) ||( a ==b && b == d) || ( a == c && c == d ) || (b == c && c ==d ))// there is the condition for all 4 pictures can be same 
+            {
+                wins++;
+                lbl_wins.Text = " Wins : " + wins;
+                balance += Convert.ToInt32(textBox_spinPrice.Text) * 4; // if the user have all 4 pictures same user will get 4 times from spin price.
+                btn_play.Text =("Continue Playing :");
+                lbl_slotmachine.Text = "You Won ";
+            }
+            else
+            {
+                lbl_slotmachine.Text = "Try Again";
+                loss++;
+                lbl_loss.Text = "Loss : " + loss;
+                balance -= Convert.ToInt32(textBox_spinPrice.Text);
+                textBox_balance.Text = balance.ToString();
+
+                textBox_spinPrice.Text = "" ;
+                btn_play.Enabled = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_add_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            if(textBox_add.Text == "")
+            {
+                MessageBox.Show("You didn't enter any amount");
+            }
+            else if(Convert.ToInt32(textBox_add.Text) <=0 )
+            {
+                MessageBox.Show("Invalid balance entered ! ");
+            }
+            else
+            {
+                textBox_balance.Text = (Convert.ToInt32(textBox_balance.Text) + Convert.ToInt32(textBox_add.Text)).ToString();
+            }
+        }// working well too.
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            move++;
+            if (move < 30)
+            {
+
+                a = rnd.Next(4) + 1;
+                b = rnd.Next(4) + 1;
+                c = rnd.Next(4) + 1;
+                d = rnd.Next(4) + 1;
+                
+
+                switch (a)
+                {
+                    case 1:
+                        pictureBox1.Image = Properties.Resources.imgbin_casino_gambling_slot_machine_icon_diamond_chips_1siX49jPrGEKifb1kWNyzSchP;
+                        break;
+                    case 2:
+                        pictureBox1.Image = Properties.Resources.gold_casino_slot_machine_with_cherry_symbol_icon_isolated_on_black_background_gambling_games_vector_700_2697232791;
+                        break;
+                    case 3:
+                        pictureBox1.Image = Properties.Resources.images;
+                        break;
+                    case 4:
+                        pictureBox1.Image = Properties.Resources.dollar;
+                        break; // working well... It won't be stop because we didn't have anything for it. So let's make it for another pictures too.
+                }
+                switch (b)
+
+                {
+                    case 1:
+                        pictureBox2.Image = Properties.Resources.imgbin_casino_gambling_slot_machine_icon_diamond_chips_1siX49jPrGEKifb1kWNyzSchP;
+                                    break;
+                    case 2:
+                        pictureBox2.Image = Properties.Resources.gold_casino_slot_machine_with_cherry_symbol_icon_isolated_on_black_background_gambling_games_vector_700_2697232791;
+                        break;
+                    case 3:
+                        pictureBox2.Image = Properties.Resources.images;
+                        break;
+                    case 4:
+                        pictureBox2.Image = Properties.Resources.dollar;
+                        break;
+                }
+                switch (c)
+                {
+                    case 1:
+                        pictureBox3.Image = Properties.Resources.imgbin_casino_gambling_slot_machine_icon_diamond_chips_1siX49jPrGEKifb1kWNyzSchP;
+                                      break;
+                    case 2:
+                        pictureBox3.Image = Properties.Resources.gold_casino_slot_machine_with_cherry_symbol_icon_isolated_on_black_background_gambling_games_vector_700_2697232791;
+                        break;
+                    case 3:
+                        pictureBox3.Image = Properties.Resources.images;
+                        break;
+                    case 4:
+                        pictureBox3.Image = Properties.Resources.dollar;
+                        break;
+                }
+                switch (d)
+
+                {
+                    case 1:
+                        pictureBox4.Image = Properties.Resources.imgbin_casino_gambling_slot_machine_icon_diamond_chips_1siX49jPrGEKifb1kWNyzSchP;
+                                    break;
+                    case 2:
+                        pictureBox4.Image = Properties.Resources.gold_casino_slot_machine_with_cherry_symbol_icon_isolated_on_black_background_gambling_games_vector_700_2697232791;
+                        break;
+                    case 3:
+                        pictureBox4.Image = Properties.Resources.images;
+                        break;
+                    case 4:
+                        pictureBox4.Image = Properties.Resources.dollar;
+                        break; //working nicely :)
+                }
+            }
+            else
+            {
+                timer1.Enabled = false;
+                move = 0; // it will stop our pictures. 
+                Game_Result(); // calling function 
+            }
+        }
+        
+
+
+        private void btn_play_Click(object sender, EventArgs e)
+        {
+            if (textBox_spinPrice.Text == "")
+            {
+                MessageBox.Show("Please press the add balance button and enter your amount how much you want to put... ");
+
+            }
+            else if (Convert.ToInt32(textBox_spinPrice.Text) <= 0) // if the user enter price less than 0 
+            {
+                MessageBox.Show("Invalid deposit entered");
+            }
+            else if (Convert.ToInt32(textBox_spinPrice.Text) > Convert.ToInt32(textBox_balance.Text)) // if the user enter amount more than balance
+            {
+                MessageBox.Show("Insufficent balance ! ");
+            }
+            else                                                
+            {
+                timer1.Enabled = true;
+                lbl_slotmachine.Text = "Casino";
+                textBox_spinPrice.Enabled = false;
+                textBox_spinPrice.BackColor = Color.LightBlue; // this code will change the background on spinprice box than we can undertand everything is okey 
+                // Play button working well...
+
+                // Everything is working perfectly :):)
+                // Thanks for your time and watching my first c# project :)
+            }
+        }
+    }
+}
